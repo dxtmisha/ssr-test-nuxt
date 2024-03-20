a1972fba8094486fccf45e00da88fb31a6fe52c4
dxtmisha <9943154+dxtmisha@users.noreply.github.com>
2024-03-20 13:33:41 +0700

    Исправления description

--- pages/description.vue
+++ pages/description.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-const {data} = await useAsyncData<{ data: { title: string, description: string } }>(
+const {data} = await useAsyncData<{ data: { title: string, description: string, code: string } }>(
     'vukhtantu',
     () => $fetch('https://symfony.f56go.com/vukhtantu/description')
 )
 
 const title = computed(() => data.value && data.value.data.title)
 const description = computed(() => data.value && data.value.data.description)
+const code = computed(() => data.value && data.value.data.code)
+
+useHead({
+  title: title.value,
+  meta: [{name: 'description', content: description}],
+})
 </script>
 
 <template>
@@ -14,7 +20,7 @@ const description = computed(() => data.value && data.value.data.description)
   </div>
   <div v-if="data">
     <h1>{{ title }}</h1>
-    <div v-html="description"></div>
+    <div v-html="code"></div>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
-const {data} = await useAsyncData<{ data: { title: string, description: string } }>(
+const {data} = await useAsyncData<{ data: { title: string, description: string, code: string } }>(
     'vukhtantu',
     () => $fetch('https://symfony.f56go.com/vukhtantu')
 )
 
 const title = computed(() => data.value && data.value.data.title)
 const description = computed(() => data.value && data.value.data.description)
+const code = computed(() => data.value && data.value.data.code)
+
+useHead({
+  title,
+  meta: [{name: 'description', content: description}],
+})
 </script>
 
 <template>
   <div v-if="data">
     <h1>{{ title }}</h1>
-    <div v-html="description"></div>
+    <div v-html="code"></div>
   </div>
   <NuxtLink to="/description">description</NuxtLink>
 </template>
